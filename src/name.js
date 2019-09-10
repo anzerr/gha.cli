bc05cf47edd781d589646363b660f1580262e1fc
anzerr <ryan.anzer@epitech.eu>
2019-09-10 14:21:24 +0200

    🚧 dump

--- src/name.js
+++ src/name.js
@@ -5,7 +5,7 @@ module.exports = (p, scope, version) => {
 	return fs.readFile(p).then((res) => {
 		let pack = JSON.parse(res.toString());
 		if (scope) {
-			return `@${(scope)}/${pack.name.replace(/(@\w+\/)+/, '')}:${version || pack.version}`;
+			return `${(scope)}/${pack.name.replace(/(@\w+\/)+/, '')}:${version || pack.version}`;
 		}
 		return `${pack.name}:${version || pack.version}`;
 	});
