a4a89063c11c6d542e6c0df234009a575043b726
anzerr <ryan.anzer@epitech.eu>
2019-09-10 14:36:27 +0200

    🚧 dump

--- src/name.js
+++ src/name.js
@@ -1,12 +1,12 @@
 
 const fs = require('fs.promisify');
 
-module.exports = (p, scope, version, tag) => {
+module.exports = (p, scope, version, t) => {
 	return fs.readFile(p).then((res) => {
-		let pack = JSON.parse(res.toString());
+		const pack = JSON.parse(res.toString()), tag = (t) ? '/' + t : '';
 		if (scope) {
-			return `${(scope)}/${pack.name.replace(/(@\w+\/)+/, '')}:${version || pack.version}`;
+			return `${(scope)}/${pack.name.replace(/(@\w+\/)+/, '')}${tag}:${version || pack.version}`;
 		}
-		return `${pack.name}${(tag) ? '/' + tag : ''}:${version || pack.version}`;
+		return `${pack.name}${tag}:${version || pack.version}`;
 	});
 };
